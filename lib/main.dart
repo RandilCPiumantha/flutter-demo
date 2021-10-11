b1a983d1782b5e404f42ef214893dd6c8e77476a
RandilCPiumantha <cst17009@std.uwu.ac.lk>
2021-10-11 08:22:23 +0530

    add checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -16,13 +17,20 @@ class _State extends State<MyApp>{
 
   String _value = " ";
   int _num1 = 0;
+  bool? _value1 = false;
+  bool? _value2 = false;
+
+
+  void _value1Changed(bool? value) => setState(() => _value1 = value);
+  void _value2Changed(bool? value) => setState(() => _value2 = value);
+
 
   void _onChange(String value){
-    setState(() =>_value = "Change ${_value}");
+    setState(() =>_value = "Change ${value}");
   }
 
   void _onSubmit(String value){
-    setState(() =>_value = "Submit ${_value}");
+    setState(() =>_value = "Submit ${value}");
   }
 
   void _add(){
@@ -53,10 +61,22 @@ class _State extends State<MyApp>{
         padding: new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
-            children: [
+            children:<Widget> [
               new Text(_value),
-              new TextField()
-
+              new TextField(
+                decoration: new InputDecoration(
+                  labelText: 'Hello',
+                  hintText: 'Hint',
+                  icon: new Icon(Icons.people)
+                ),
+                autocorrect: true,
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                onChanged: _onChange,
+                onSubmitted: _onSubmit,
+              ),
+            new Checkbox(value:_value1, onChanged:_value1Changed),
+              new Checkbox(value:_value2, onChanged:_value2Changed),
             ],
           ),
         ),
